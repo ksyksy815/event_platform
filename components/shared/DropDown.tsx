952a5b047a9056676099e14eafd71f694b28e595
Seoyeon Kim <seoyeonkim815@gmail.com>
2023-12-31 14:42:55 +0900

    Event form UI

---
+++ components/shared/DropDown.tsx
@@ -0,0 +1,81 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+import { useState } from "react";
+import { Input } from "../ui/input";
+
+type DropDownProps = {
+  onChangeHandler: (value: string) => void;
+  value?: string;
+};
+
+const DropDown = ({ onChangeHandler, value }: DropDownProps) => {
+  const [categories, setCategories] = useState<ICategory[]>([]);
+  const [newCategory, setNewCategory] = useState<string>("");
+
+  const handleAddCategory = () => {};
+
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 &&
+          categories.map((category) => (
+            <SelectItem
+              key={category._id}
+              value={category._id}
+              className="select-item p-regular-14">
+              {category.name}
+            </SelectItem>
+          ))}
+
+        <AlertDialog>
+          <AlertDialogTrigger
+            className={`p-medium-14 flex w-full rouunded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500`}>
+            Open
+          </AlertDialogTrigger>
+          <AlertDialogContent className="bg-white">
+            <AlertDialogHeader>
+              <AlertDialogTitle>New Category</AlertDialogTitle>
+              <AlertDialogDescription>
+                <Input
+                  type="text"
+                  placeholder="Category name"
+                  className="input-field mt-3"
+                  onChange={(e) => setNewCategory(e.target.value)}
+                />
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => startTransition(handleAddCategory)}>
+                Add
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default DropDown;
